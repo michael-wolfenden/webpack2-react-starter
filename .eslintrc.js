4199b0529bdc69e0db24e88216af1e6722d1f15c
Michael Wolfenden <michael-wolfenden@users.noreply.github.com>
2016-07-08 14:03:23 +1000

    updated config file to match eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "extends": "airbnb",
-    "plugins": [
-        "react"
+    parser: 'babel-eslint',
+    extends: 'airbnb',
+    plugins: [
+        'react',
     ],
-    "rules": {
+    rules: {
         // http://eslint.org/docs/rules/semi
         // no semi-colons (YOLO) .. if you really want semicolons, remove this rule and run
         // '.\node_modules\.bin\eslint --fix src' from the app root to re-add
-        "semi": [2, "never"],
+        semi: [2, 'never'],
         // http://eslint.org/docs/rules/indent
         // indent 4 spaces (rather than airbnb's default of 2)
-        "indent": [2, 4, { "SwitchCase": 1 }],
+        indent: [2, 4, { SwitchCase: 1 }],
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent.md
         // indent 4 spaces (rather than airbnb's default of 2)
-        "react/jsx-indent": [2, 4],
+        'react/jsx-indent': [2, 4],
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent-props.md
         // indent 4 spaces (rather than airbnb's default of 2)
-        "react/jsx-indent-props": [2, 4],
+        'react/jsx-indent-props': [2, 4],
     },
-    "env": {
-        "mocha": true
+    env: {
+        mocha: true,
     },
-    "settings": {
-        "import/resolver": {
-            "webpack": {
-                "config": "config/webpack.config.development.js"
-            }
-        }
-    }
+    settings: {
+        'import/resolver': {
+            webpack: {
+                config: 'config/webpack.config.development.js',
+            },
+        },
+    },
 }
